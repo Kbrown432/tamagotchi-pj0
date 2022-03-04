f048a283e1ddc0940d998057dbbcbbf9f94202bc
Kbrown432 <Kbrown1193@gmail.com>
2022-03-04 12:10:11 -0500

    created button variables

---
+++ js/app.js
@@ -0,0 +1,7 @@
+// create variables for my button elements
+const king = document.querySelector('.king');
+const food = document.querySelector('.action');
+const play = document.querySelector('.fun');
+const rest = document.querySelector('.sleep');
+
+// create a click operator for king, food, play & rest buttons
